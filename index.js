d5be70b605f386c35afdfb43e8ac7eb66d271d45
paprisko <jackmchussein965@gmail.com>
2025-03-02 15:40:06 +0300

    phase-0-javascript-events-event-listening-lab

--- index.js
+++ index.js
@@ -1,2 +1,10 @@
 function addingEventListener() {
-}
+    const input = document.getElementById("button");
+  
+    if (input) {
+      input.addEventListener("click", function () {
+        alert("I was clicked!");
+      });
+    }
+  }
+  
